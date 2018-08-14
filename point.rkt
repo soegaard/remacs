0682a407943bf3f601f7c3ca5ee553fc938baf8c
Jens Axel Søgaard <jensaxel@soegaard.net>
2018-08-14 22:54:26 +0200

    Add comments.

--- point.rkt
+++ point.rkt
@@ -4,6 +4,15 @@
 ;;; POINT
 ;;;
 
+; Each buffer has a point. The point is the position where character and strings
+; insertions happen. The point is represented as a mark.
+; Use  get-point         to get the mark representing the point.
+; Use  point             to get the position of the mark.
+; Use  with-saved-point  to preserverve the point placement.
+; If narrowing is in effect, the point movement is restricted.
+; The functions  point-min  and  point-max  in "narrowing.rkt" returns
+; the minimum and maximum point positions.
+
 (require (for-syntax racket/base syntax/parse "parameters.rkt" "representation.rkt")
          racket/list
          "mark.rkt"
@@ -11,10 +20,12 @@
          "representation.rkt")
 
 ; get-point : window -> mark
+;   get the mark that represent the point
 (define (get-point [b (current-buffer)])
   (buffer-point b))
 
 ; point : window -> integer
+;   get the position of the mark
 (define (point [b (current-buffer)])
   (mark-position (buffer-point b)))
 
@@ -25,6 +36,11 @@
     [m    (mark-move-to-position! m (position pos))]
     [else (mark-move-to-position! (get-point) (position pos))]))
 
+
+; SYNTAX  (with-saved-point body ...)
+;   Save the position of the body.
+;   Evaluate the body.
+;   Restore the original position of the point.
 (define-syntax (with-saved-point stx)
   (syntax-parse stx
     [(_with-saved-point body ...)
